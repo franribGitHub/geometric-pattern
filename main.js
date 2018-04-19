6d3ba0789c90b25116d4569115cd05b7f5dbe3f4
franrib <franrib@bellsouth.net>
2018-04-19 19:58:45 +0000

    Add colors

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
+var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
 function setup() {
     createCanvas(480, 600);
     circleDiameter = width/NUM_CIRCLES;
+    circleRadius = circleDiameter/2;
 }
 
 function draw() {
-    var y = 0;
-    while (y <= height) {
-        var x = 0;
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
+    
+    var isShifted = false;
+    
+    var y = height;
+    while (y >= 0) {
+        
+        var x;
+        
+        if (isShifted) {
+            x = circleRadius;
+        } else {
+            x = 0
+        }
+        
         while (x <= width) {
-            ellipse(x, 0, circleDiameter, circleDiameter);
+            
+            fill(color(rVal, bVal, gVal));
+            stroke(color(rVal, bVal, gVal));
+            
+            ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
-        y = y + circleDiameter;
+        
+        y = y - circleRadius;
+        isShifted = !isShifted;
+        
+        rVal = rVal - 8;
+        gVal = gVal + 14;
+        bVal = bVal + 6;
     }
 }
